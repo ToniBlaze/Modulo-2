a5abd7e6db40e109367c28bac93db382f55cc5aa
Alessio <alessio.toninello@gmail.com>
2023-02-06 20:03:43 +0100

    quasi finito, manca solo es18

--- D4/main.js
+++ D4/main.js
@@ -17,27 +17,30 @@ console.log(me)
   Scrivi del codice per creare un array di soli valori DISPARI da 1 a 100.
  */
 
-let array = []
+let array = [];
 for (let i = 0; i <= 100; i++) {
   if (i%2 === 0) {
   continue;
   } else {
   array.push(i);
-  console.log(array)
   }
-}
+} console.log(array)
 
 
 /* ESERCIZIO 3
   Scrivi del codice per creare un array di 10 elementi; ognuno di essi deve essere un valore random compreso tra 0 e 100 (incluso).
  */
 
-let arrayLenght = 10
-const randomArray = []
-for(let i = 0; i < arrayLenght; i++) {randomArray.push(Math.random()*100)}
-console.log(randomArray)
-
-/* QUESTO NON LO ABBIAMO VISTO! CHE SENSO HA METTERLO NEGLI ESERCIZI? */
+arr = [];
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
+}
+for (let i = 0; i < 100; i++) {
+  arr.push(getRandomInt(0, 100));
+}
+console.log(arr);
 
 
 
@@ -45,16 +48,15 @@ console.log(randomArray)
   Scrivi del codice per ricavare solamente i valori PARI da un array composto da soli valori numerici.
  */
 
-let arr = [4, 61, 94, 11, 13, 146, 9, 34, 89, 46, 72, 7];
+let arr2 = [4, 61, 94, 11, 13, 146, 9, 34, 89, 46, 72, 7];
 let evenNumber = [];
 
-for (let i = 0; i < arr.length; i++) {
-  if (arr[i]%2 == 0) {
-    evenNumber.push(arr[i]);
-}
-}
+for (let i = 0; i < arr2.length; i++) {
+  if (arr2[i] % 2 == 0) {
+    evenNumber.push(arr2[i]);
+  }
+} console.log(evenNumber);
 
-/* NON SO FARLO!!!! */
 
 
 
@@ -62,17 +64,36 @@ for (let i = 0; i < arr.length; i++) {
   Scrivi del codice per sommare a catena i numeri contenuti in un array.
  */
 
+let x = [45, 9, 26, 4];
+let y = ["ciao", "Alessio", "Sabato"]
 
+let total = x.concat(y)
+console.log(total);
 
 /* ESERCIZIO 6
   Scrivi del codice per incrementare di 1 tutti i valori numerici in un array.
 */
 
+let arr3 = [4, 94, 82, 14, 3, 150]
+
+for (let i = 0; i < arr3.length; i++) {
+  arr3[i] += 1
+} console.log(arr3);
 
 
 /* ESERCIZIO 7 (EXTRA)
   Scrivi del codice per eliminare solo i valori PARI da un array.
 */
+
+let arr4 = [4, 9, 125, 68, 12, 964, 32, 61, 84, 77, 99]
+let removed = []
+
+for (let i = 0; i < arr4.length; i++) {
+if (arr4[i] % 2 === 0) {
+  removed.push(arr4[i])
+  }
+} console.log(removed);
+
 
 
 
@@ -80,6 +101,10 @@ for (let i = 0; i < arr.length; i++) {
   Scrivi del codice per creare un array di 10 elementi; ognuno di essi deve essere un valore random compreso tra 0 e 10 (incluso), SENZA AMMETTERE DUPLICATI.
  */
 
+  let arrayLenght = 10
+  const randomArray = []
+  for(let i = 0; i < arrayLenght; i++) {randomArray.push(Math.random()*10)}
+  console.log(randomArray)
 
 
 /* ESERCIZIO 9
@@ -87,19 +112,35 @@ for (let i = 0; i < arr.length; i++) {
   es.: ["EPICODE", "is", "great"] => [7, 2, 5]
 */
 
+let arr5 = ["WebDeveloper","Ciao", "Programmazione", "javascript"];
 
+for (i = 0; i < arr5.length; i++) {
+  arr5[i] = arr5[i].length;
+}
+console.log(arr5);
 
 /* ESERCIZIO 10
   Scrivi un algoritmo in grado di invertire un array.
   es: [1, 3, 5] ==> [5, 3, 1]
 */
 
+let arr6 = ["ciao", 5, "eiffel65", true, "xxx", 100]
+
+let arrReverse = arr6.reverse()
+console.log(arrReverse);
 
 
 /* ESERCIZIO 11
   Scrivi del codice per estrarre il massimo valore numerico da un array.
 */
 
+let arr7 = [32, 2, 5064, 199, 463, 10000, 6]
+let maxNumb = 0
+for (let i = 0; i < arr7.length; i++) {
+  if (arr7[i] > maxNumb) {
+    maxNumb = arr7[i];
+  }  
+} console.log(maxNumb);
 
 
 /* Questo array di film verrà usato negli esercizi a seguire. Non modificarlo e scorri oltre per riprendere gli esercizi :) */
@@ -221,27 +262,82 @@ const movies = [
 /* ESERCIZIO 12
   Scrivi del codice per trovare il film più vecchio nell'array fornito.
 */
+let oldestFilm;
+
+for (let i = 0; i < movies.length; i++) {
+  const oldest = movies[i];
+  if (oldestFilm) {
+    if (oldest.Year < oldestFilm.Year) {
+      oldestFilm = oldest;
+    } else {
+    }
+  } else {
+    oldestFilm = oldest;
+  }
+}
+console.log(oldestFilm);
+
 
 /* ESERCIZIO 13
   Scrivi del codice per ottenere il numero di film contenuti nell'array fornito.
 */
 
+console.log(movies.length);
+
 /* ESERCIZIO 14
   Scrivi del codice per creare un array con solamente i titoli dei film contenuti nell'array fornito.
 */
+
+let titles = [];
+
+for (let i = 0; i < movies.length; i++) {
+  titles.push(movies[i].Title);
+}
+console.log(titles);
+
+
 
 /* ESERCIZIO 15
   Scrivi del codice per ottenere dall'array fornito solamente i film usciti nel millennio corrente.
 */
 
+let films2000 = [];
+
+for (let i=0; i < movies.length; i++) {
+  if (movies[i].Year > 1999) {
+    films2000.push(movies[i])
+  } 
+} console.log(films2000);
+
+
+
+
 /* ESERCIZIO 16
   Scrivi del codice per ottenere dall'array fornito il film con il seguente id.
 */
 const id = 'tt0355702'
+let idFilm = []
+
+for (let i = 0; i < movies.length; i++) {
+  if (movies[i].imdbID === id) {
+    idFilm.push(movies[i])
+  }
+} console.log(idFilm);
+
 
 /* ESERCIZIO 17
   Scrivi del codice per calcolare la somma di tutti gli anni in cui sono stati prodotti i film contenuti nell'array fornito.
 */
+
+let sum = 0;
+let parseNumb = 0;
+
+for (let i = 0; i < movies.length; i++) {
+  parseNumb = parseInt(movies[i].Year);
+  sum = sum + parseNumb;      
+}
+console.log(sum);
+
 
 /* ESERCIZIO 18
   Scrivi del codice per recuperare tutti i film dall'array fornito che contengono una parola fornita.
