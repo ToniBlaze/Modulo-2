98d42d9736f684e2008f70e33e910b0f572b15a6
Alessio <alessio.toninello@gmail.com>
2023-03-16 21:18:46 +0100

    modificato es.27

--- D8/Esercizio/main.js
+++ D8/Esercizio/main.js
@@ -52,19 +52,34 @@ console.log(arrayMax, arrayMin);
 
 // 28) Crea un array di array, nel quale ogni array "figlio" ha 10 numeri random
 
-arrayFather = [
-    [1,2,3,4,5,6,7,8,9,10],
-    [11,12,13,14,15,16,17,18,19,20],
-    [21,22,23,24,25,26,27,28,29,30],
-    [31,32,33,34,35,36,37,38,39,40],
-    [41,42,43,44,45,46,47,48,49,50],
-    [51,52,53,54,55,56,57,58,59,60],
-    [61,62,63,64,65,66,67,68,69,70],
-    [71,72,73,74,75,76,77,78,79,80],
-    [81,82,83,84,85,86,87,88,89,90],
-    [91,92,93,94,95,96,97,98,99,100] 
-]
-console.log(arrayFather);
+// arrayFather = [
+//     [1,2,3,4,5,6,7,8,9,10],
+//     [11,12,13,14,15,16,17,18,19,20],
+//     [21,22,23,24,25,26,27,28,29,30],
+//     [31,32,33,34,35,36,37,38,39,40],
+//     [41,42,43,44,45,46,47,48,49,50],
+//     [51,52,53,54,55,56,57,58,59,60],
+//     [61,62,63,64,65,66,67,68,69,70],
+//     [71,72,73,74,75,76,77,78,79,80],
+//     [81,82,83,84,85,86,87,88,89,90],
+//     [91,92,93,94,95,96,97,98,99,100] 
+// ]
+// console.log(arrayFather);
+
+function getRandomNumbers2() {
+    const array10 = []
+    for (let i = 0; i < 10; i++) {
+        arrayRandom.push(Math.floor(Math.random() * 100))        
+    }
+    return array10
+}
+
+const arrayOfArray = []
+for (let i = 0; i < 3; i++) {
+    arrayOfArray.push(getRandomNumbers2())   
+}
+
+console.log("array: " , arrayOfArray)
 
 // 29) Crea una funzione che riceve 2 array come parametri e ritorni quello con più elementi
 
@@ -78,6 +93,7 @@ function longestArray(first, second) {
         return second
     }
 }
+
 console.log(longestArray(arr1,arr2))
 
 
